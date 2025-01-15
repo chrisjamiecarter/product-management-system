0636e923893798de5264a73ed581366ec5eef70e
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-01-15 14:12:00 +0000

    Added create product command and handler

---
+++ src/ProductManagement.Application/Products/Commands/CreateProduct/CreateProductCommand.cs
@@ -0,0 +1,8 @@
+﻿using ProductManagement.Application.Abstractions.Messaging;
+
+namespace ProductManagement.Application.Products.Commands.CreateProduct;
+
+public sealed record CreateProductCommand(
+    string Name,
+    string Description,
+    decimal Price) : ICommand;
---
+++ src/ProductManagement.Application/Products/Commands/CreateProduct/CreateProductCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using ProductManagement.Application.Abstractions.Messaging;
+using ProductManagement.Application.Errors;
+using ProductManagement.Domain.Entities;
+using ProductManagement.Domain.Repositories;
+using ProductManagement.Domain.Shared;
+using ProductManagement.Domain.ValueObjects;
+
+namespace ProductManagement.Application.Products.Commands.CreateProduct;
+
+internal sealed class CreateProductCommandHandler : ICommandHandler<CreateProductCommand>
+{
+    private readonly IProductRepository _productRepository;
+
+    public CreateProductCommandHandler(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public async Task<Result> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        var name = ProductName.Create(request.Name);
+        var description = request.Description;
+        var price = ProductPrice.Create(request.Price);
+
+        if (name.IsFailure)
+        {
+            return Result.Failure(name.Error);
+        }
+
+        if (price.IsFailure)
+        {
+            return Result.Failure(price.Error);
+        }
+
+        var product = new Product(
+            Guid.CreateVersion7(),
+            name.Value,
+            description,
+            price.Value);
+
+        var isCreated = await _productRepository.CreateAsync(product, cancellationToken);
+
+        return isCreated ? Result.Success() : Result.Failure(ApplicationErrors.Product.NotCreated);
+    }
+}
