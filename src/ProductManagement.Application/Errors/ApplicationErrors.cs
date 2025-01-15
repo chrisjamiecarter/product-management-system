3963a1586dfdaa89e7591c1f299fa05e3dce5358
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-01-15 14:10:29 +0000

    Added product ApplicationErrors

---
+++ src/ProductManagement.Application/Errors/ApplicationErrors.cs
@@ -0,0 +1,25 @@
+﻿using ProductManagement.Domain.Shared;
+
+namespace ProductManagement.Application.Errors;
+
+internal static class ApplicationErrors
+{
+    internal static class Product
+    {
+        internal static readonly Error NotCreated = new(
+            "Product.NotCreated",
+            "The product was not created.");
+
+        internal static readonly Error NotDeleted = new(
+            "Product.NotDeleted",
+            "The product was not deleted.");
+
+        internal static readonly Error NotFound = new(
+            "Product.NotFound",
+            "The product with Id was not found.");
+        
+        internal static readonly Error NotUpdated = new(
+            "Product.NotUpdated",
+            "The product was not updated.");
+    }
+}
