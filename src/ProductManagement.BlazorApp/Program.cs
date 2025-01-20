9c183ce73e365850a2a1c71c9026ad644dee3314
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-01-20 16:16:26 +0000

    Implemented roles and authorisation

--- src/ProductManagement.BlazorApp/Program.cs
+++ src/ProductManagement.BlazorApp/Program.cs
@@ -12,7 +12,7 @@ namespace ProductManagement.BlazorApp;
 
 public class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
         builder.Services.AddApplication();
@@ -37,31 +37,35 @@ public class Program
         })
         .AddIdentityCookies();
 
+        builder.Services.AddAuthorizationBuilder()
+            .AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"))
+            .AddPolicy("RequireUserRole", policy => policy.RequireRole("User"));
+
         var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
         builder.Services.AddDbContext<ApplicationDbContext>(options =>
         {
             options.UseSqlServer(connectionString);
-            options.UseSeeding((context, _) =>
-            {
-                var defaultAdminEmail = "admin@email.com";
-                var defaultAdminUser = new ApplicationUser
-                {
-                    UserName = defaultAdminEmail,
-                    NormalizedUserName = defaultAdminEmail.ToUpper(),
-                    Email = defaultAdminEmail,
-                    NormalizedEmail = defaultAdminEmail.ToUpper(),
-                    EmailConfirmed = true,
-                };
+            //options.UseSeeding((context, _) =>
+            //{
+            //    var defaultAdminEmail = "admin@email.com";
+            //    var defaultAdminUser = new ApplicationUser
+            //    {
+            //        UserName = defaultAdminEmail,
+            //        NormalizedUserName = defaultAdminEmail.ToUpper(),
+            //        Email = defaultAdminEmail,
+            //        NormalizedEmail = defaultAdminEmail.ToUpper(),
+            //        EmailConfirmed = true,
+            //    };
 
-                var contains = context.Set<ApplicationUser>().Any(x => x.UserName == defaultAdminUser.UserName);
-                if (!contains)
-                {
-                    defaultAdminUser.PasswordHash = new PasswordHasher<ApplicationUser>().HashPassword(defaultAdminUser, "Admin123###");
-                    context.Set<ApplicationUser>().Add(defaultAdminUser);
-                    context.SaveChanges();
-                }
+            //    var contains = context.Set<ApplicationUser>().Any(x => x.UserName == defaultAdminUser.UserName);
+            //    if (!contains)
+            //    {
+            //        defaultAdminUser.PasswordHash = new PasswordHasher<ApplicationUser>().HashPassword(defaultAdminUser, "Admin123###");
+            //        context.Set<ApplicationUser>().Add(defaultAdminUser);
+            //        context.SaveChanges();
+            //    }
 
-            });
+            //});
         });
         builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
@@ -74,6 +78,7 @@ public class Program
             options.Password.RequireNonAlphanumeric = true;
             options.SignIn.RequireConfirmedAccount = true;
         })
+        .AddRoles<IdentityRole>()
         .AddEntityFrameworkStores<ApplicationDbContext>()
         .AddSignInManager()
         .AddDefaultTokenProviders();
@@ -87,6 +92,50 @@ public class Program
         {
             var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
             dbContext.Database.Migrate();
+
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            var userStore = scope.ServiceProvider.GetRequiredService<IUserStore<ApplicationUser>>();
+            var emailStore = (IUserEmailStore<ApplicationUser>)userStore;
+            var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var roleStore = scope.ServiceProvider.GetRequiredService<IRoleStore<IdentityRole>>();
+
+            if(!await roleManager.RoleExistsAsync("Admin"))
+            {
+                var role = Activator.CreateInstance<IdentityRole>();
+                await roleStore.SetRoleNameAsync(role, "Admin", CancellationToken.None);
+                var roleResult = await roleManager.CreateAsync(role);
+            }
+
+            if (!await roleManager.RoleExistsAsync("User"))
+            {
+                var role = Activator.CreateInstance<IdentityRole>();
+                await roleStore.SetRoleNameAsync(role, "User", CancellationToken.None);
+                var roleResult = await roleManager.CreateAsync(role);
+            }
+
+            var adminEmail = "admin@email.com";
+            var adminPassword = "Admin123###";
+            if (await userManager.FindByEmailAsync(adminEmail) is null)
+            {
+                var user = Activator.CreateInstance<ApplicationUser>();
+                await userStore.SetUserNameAsync(user, adminEmail, CancellationToken.None);
+                await emailStore.SetEmailAsync(user, adminEmail, CancellationToken.None);
+                await emailStore.SetEmailConfirmedAsync(user, true, CancellationToken.None);
+                var userResult = await userManager.CreateAsync(user, adminPassword);
+                var roleResult = await userManager.AddToRoleAsync(user, "Admin");
+            }
+
+            var userEmail = "user@email.com";
+            var userPassword = "User123###";
+            if (await userManager.FindByEmailAsync(userEmail) is null)
+            {
+                var user = Activator.CreateInstance<ApplicationUser>();
+                await userStore.SetUserNameAsync(user, userEmail, CancellationToken.None);
+                await emailStore.SetEmailAsync(user, userEmail, CancellationToken.None);
+                await emailStore.SetEmailConfirmedAsync(user, true, CancellationToken.None);
+                var userResult = await userManager.CreateAsync(user, userPassword);
+                var roleResult = await userManager.AddToRoleAsync(user, "User");
+            }
         }
 
         if (app.Environment.IsDevelopment())
