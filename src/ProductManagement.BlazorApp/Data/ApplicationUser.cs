6ab6c82c45abf05b88b03899386d9fdff388ba2b
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-01-20 16:16:41 +0000

    Code cleanup

--- src/ProductManagement.BlazorApp/Data/ApplicationUser.cs
+++ src/ProductManagement.BlazorApp/Data/ApplicationUser.cs
@@ -1,8 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 
 namespace ProductManagement.BlazorApp.Data;
-// Add profile data for application users by adding properties to the ApplicationUser class
+
 public class ApplicationUser : IdentityUser
 {
 }
-
