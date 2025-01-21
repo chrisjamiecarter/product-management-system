f1a01ee23d89b8c1b516e7f885001c413c66c670
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-01-21 20:19:44 +0000

    Implemented users list page

--- src/ProductManagement.Application/Users/Queries/GetUsersPaginated/GetUsersPaginatedQuery.cs
+++ src/ProductManagement.Application/Users/Queries/GetUsersPaginated/GetUsersPaginatedQuery.cs
@@ -5,7 +5,6 @@ namespace ProductManagement.Application.Users.Queries.GetUsersPaginated;
 
 public sealed record GetUsersPaginatedQuery(string? SearchUserName,
                                             bool? SearchEmailConfirmed,
-                                            string? SortColumn,
                                             string? SortOrder,
                                             int PageNumber = 1,
                                             int PageSize = 10) : IQuery<PaginatedList<GetUsersPaginatedQueryResponse>>;
---
+++ src/ProductManagement.BlazorApp/Components/Users/Models/SearchUsersDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductManagement.BlazorApp.Components.Users.Models;
+
+public class SearchUsersDto
+{
+    [DataType(DataType.Text)]
+    public string? SearchUserName { get; set; }
+
+    public bool? SearchEmailConfirmed { get; set; }
+
+    public string? SortOrder { get; set; }
+}
