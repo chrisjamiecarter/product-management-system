f946719275905146d95ae8c76e497d4276e8423d
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-01-22 15:37:56 +0000

    Removed redundant code

--- src/ProductManagement.BlazorApp/Program.cs
+++ src/ProductManagement.BlazorApp/Program.cs
@@ -40,47 +40,6 @@ public class Program
             .AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"))
             .AddPolicy("RequireUserRole", policy => policy.RequireRole("User"));
 
-        //var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-        //builder.Services.AddDbContext<ApplicationDbContext>(options =>
-        //{
-        //    options.UseSqlServer(connectionString);
-        //    //options.UseSeeding((context, _) =>
-        //    //{
-        //    //    var defaultAdminEmail = "admin@email.com";
-        //    //    var defaultAdminUser = new ApplicationUser
-        //    //    {
-        //    //        UserName = defaultAdminEmail,
-        //    //        NormalizedUserName = defaultAdminEmail.ToUpper(),
-        //    //        Email = defaultAdminEmail,
-        //    //        NormalizedEmail = defaultAdminEmail.ToUpper(),
-        //    //        EmailConfirmed = true,
-        //    //    };
-
-        //    //    var contains = context.Set<ApplicationUser>().Any(x => x.UserName == defaultAdminUser.UserName);
-        //    //    if (!contains)
-        //    //    {
-        //    //        defaultAdminUser.PasswordHash = new PasswordHasher<ApplicationUser>().HashPassword(defaultAdminUser, "Admin123###");
-        //    //        context.Set<ApplicationUser>().Add(defaultAdminUser);
-        //    //        context.SaveChanges();
-        //    //    }
-
-        //    //});
-        //});
-
-        //builder.Services.AddIdentityCore<ApplicationUser>(options =>
-        //{
-        //    options.Password.RequiredLength = 8;
-        //    options.Password.RequireDigit = true;
-        //    options.Password.RequireLowercase = true;
-        //    options.Password.RequireUppercase = true;
-        //    options.Password.RequireNonAlphanumeric = true;
-        //    options.SignIn.RequireConfirmedAccount = true;
-        //})
-        //.AddRoles<IdentityRole>()
-        //.AddEntityFrameworkStores<ApplicationDbContext>()
-        //.AddSignInManager()
-        //.AddDefaultTokenProviders();
-
         builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
 
         var app = builder.Build();
