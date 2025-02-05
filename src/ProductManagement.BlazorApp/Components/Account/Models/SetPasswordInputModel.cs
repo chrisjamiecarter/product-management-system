e3c893152418ffd635edefc5bb6948fd793795e4
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-02-05 22:13:38 +0000

    Refactored ChangeEmail, ChangePassword, Index, and SetPassword components to improve form layout and user experience.

--- src/ProductManagement.BlazorApp/Components/Account/Models/SetPasswordInputModel.cs
+++ src/ProductManagement.BlazorApp/Components/Account/Models/SetPasswordInputModel.cs
@@ -9,10 +9,10 @@ internal sealed class SetPasswordInputModel
     [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = PasswordOptions.RequiredLength)]
     [DataType(DataType.Password)]
     [Display(Name = "New password")]
-    public string? UpdatedPassword { get; set; }
+    public string UpdatedPassword { get; set; } = string.Empty;
 
     [DataType(DataType.Password)]
     [Display(Name = "Confirm new password")]
     [Compare("UpdatedPassword", ErrorMessage = "The new password and confirmation password do not match.")]
-    public string? ConfirmPassword { get; set; }
+    public string ConfirmPassword { get; set; } = string.Empty;
 }
